e6a85aab06987bfac39e49cc1c4f0cf819a3221f
ubuntu <matrices-m>
2020-08-21 13:49:00 +0800

    python 3

--- tf_ops/3d_interpolation/visu_interpolation.py
+++ tf_ops/3d_interpolation/visu_interpolation.py
@@ -23,12 +23,12 @@ def fun(xyz1,xyz2,pts2):
         dist = tf.maximum(dist, 1e-10)
         norm = tf.reduce_sum((1.0/dist),axis=2,keep_dims=True)
         norm = tf.tile(norm, [1,1,3])
-        print norm
+        print( norm)
         weight = (1.0/dist) / norm
         interpolated_points = three_interpolate(points, idx, weight)
     with tf.Session('') as sess:
         tmp,pts1,d,w = sess.run([xyz1, interpolated_points, dist, weight])
-        #print w
+        #print( w)
         pts1 = pts1.squeeze()
     return pts1
 
